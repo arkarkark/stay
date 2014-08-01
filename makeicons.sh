b1dd4844870670fbfb0b9c2dda8ea019c717c055
Alex Knowles <ark@looker.com>
2014-08-01 03:05:07 -0700

    transparent background

--- makeicons.sh
+++ makeicons.sh
@@ -2,5 +2,7 @@
 # brew install imagemagick
 # http://www.imagemagick.org/
 for s in 16 48 128; do
-  convert icon.png -resize "${s}x${s}" "icon${s}.png"
+  convert icon.png \
+      -resize "${s}x${s}" -background none -extent "${s}x${s}" -gravity center \
+      "icon${s}.png"
 done
